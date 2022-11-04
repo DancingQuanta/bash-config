ca74567b0f0cd90c96cf36ce77fc01190c2d3128
DancingQuanta <andytheseeker@gmail.com>
2022-11-04 13:00:14 +0000

    Updated config with example config adn retain my settings

--- .config/liquidprompt/liquid.ps1
+++ .config/liquidprompt/liquid.ps1
@@ -3,41 +3,24 @@
 # LIQUID PROMPT DEFAULT TEMPLATE FILE #
 #######################################
 
-# Available features:
-# LP_BATT battery
-# LP_LOAD load
-# LP_JOBS screen sessions/running jobs/suspended jobs
-# LP_USER user
-# LP_HOST hostname
-# LP_PERM a colon ":"
-# LP_PWD current working directory
-# LP_VENV Python virtual environment
-# LP_PROXY HTTP proxy
-# LP_VCS the content of the current repository
-# LP_ERR last error code
-# LP_MARK prompt mark
-# LP_TIME current time
-# LP_TTYN number of current terminal (useful in title for quick switching)
-# LP_RUNTIME runtime of last command
-# LP_MARK_PREFIX user-defined prompt mark prefix (helpful if you want 2-line prompts)
-# LP_PS1_PREFIX user-defined general-purpose prefix (default set a generic prompt as the window title)
-# LP_PS1_POSTFIX user-defined general-purpose postfix
-# LP_BRACKET_OPEN open bracket
-# LP_BRACKET_CLOSE close bracket
+# Available sections can be found in the documentation:
+# https://liquidprompt.readthedocs.io/en/stable/theme/default.html#templates
 
-# Remember that most features come with their corresponding colors,
-# see the README.
+# Remember that most features come with their corresponding colors.
 
-# add time
-LP_PS1="${LP_PS1_PREFIX}${LP_TIME}"
+# add time, jobs, load and battery
+LP_PS1="${LP_PS1_PREFIX}${LP_TIME}${LP_BATT}${LP_LOAD}${LP_TEMP}${LP_JOBS}"
+
 # add user, host and permissions colon
-LP_PS1="${LP_PS1}${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
-LP_PS1="${LP_PS1}${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
+LP_PS1+="${LP_BRACKET_OPEN}${LP_USER}${LP_HOST}${LP_PERM}"
+
+# add directory and venv
+LP_PS1+="${LP_PWD}${LP_BRACKET_CLOSE}${LP_VENV}${LP_PROXY}"
 
 # Add VCS infos
-LP_PS1="${LP_PS1}${LP_VCS}"
+LP_PS1+="${LP_VCS}"
 
 # add return code and prompt mark
-LP_PS1="${LP_PS1}${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
+LP_PS1+="${LP_RUNTIME}${LP_ERR}${LP_MARK_PREFIX}${LP_MARK}${LP_PS1_POSTFIX}"
 
 # vim: set et sts=4 sw=4 tw=120 ft=sh:
